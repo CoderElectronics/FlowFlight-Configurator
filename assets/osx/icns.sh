9c8fa422829b30f038133fe47dfecc89353873ff
Ari Stehney <ari.stehney@icloud.com>
2021-05-06 09:57:06 -0400

    Initial theme changes

---
+++ assets/osx/icns.sh
@@ -0,0 +1,14 @@
+input_filepath="app-icon.png"
+export output_iconset_name="$1.iconset"
+mkdir $output_iconset_name
+
+sips -z 16 16     $input_filepath --out "${output_iconset_name}/icon_16x16.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_16x16@2x.png"
+sips -z 32 32     $input_filepath --out "${output_iconset_name}/icon_32x32.png"
+sips -z 64 64     $input_filepath --out "${output_iconset_name}/icon_32x32@2x.png"
+sips -z 128 128   $input_filepath --out "${output_iconset_name}/icon_128x128.png"
+sips -z 256 256   $input_filepath --out "${output_iconset_name}/icon_128x128@2x.png"
+
+iconutil -c icns $output_iconset_name
+
+rm -R $output_iconset_name
